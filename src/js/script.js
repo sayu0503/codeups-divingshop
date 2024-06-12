ba7d973593e65bf74ac0366a7b4c41a4c9b1435f
sayu <sayu36490@gmail.com>
2024-06-12 11:05:07 +0900

    0612②

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,89 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if($(".js-hamburger").hasClass("is-active")) {
+      $(".js-hamburger").removeClass("is-active");
+    // $("html").toggleClass("is-fixed");
+    $(".js-sp-nav").fadeOut(300);
+  } else{
+  $(".js-hamburger").addClass("is-active");
+  $(".js-sp-nav").fadeIn(300);
+    }
+  });
+
+  //メインビュースライド
+    var swiper = new Swiper(".mySwiper", {
+      loop: true,
+      effect: "fade",
+      speed: 3000,
+      allowTouchMove: false,
+      autoplay: {
+        delay: 3000,
+      },
+    });
+
+   //Campaignスライド
+   var swiper = new Swiper(".js-campaign-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    centeredSlides: true,
+    width: 280,
+    breakpoints: {
+      // 768px以上の場合
+      768: {
+      slidesPerView: 3.5,
+       spaceBetween: 40,
+       centeredSlides: false,
+       width: 1265.5,
+      }
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var appendNumber = 4;
+  var prependNumber = 1;
+  document
+    .querySelector(".prepend-2-slides")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.prependSlide([
+        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      ]);
+    });
+  document
+    .querySelector(".prepend-slide")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.prependSlide(
+        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+      );
+    });
+  document
+    .querySelector(".append-slide")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.appendSlide(
+        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+      );
+    });
+  document
+    .querySelector(".append-2-slides")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      swiper.appendSlide([
+        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      ]);
+    });
 
 });
