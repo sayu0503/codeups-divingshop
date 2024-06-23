729366eb8668cde6e254ea542ac507c23cd7af39
sayu <sayu36490@gmail.com>
2024-06-23 21:26:45 +0900

    0623

--- src/js/script.js
+++ src/js/script.js
@@ -62,69 +62,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       prevEl: ".swiper-button-prev",
     },
   });
-
-  var appendNumber = 4;
-  var prependNumber = 1;
-  document
-    .querySelector(".prepend-2-slides")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.prependSlide([
-        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-      ]);
-    });
-  document
-    .querySelector(".prepend-slide")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.prependSlide(
-        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-      );
-    });
-  document
-    .querySelector(".append-slide")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.appendSlide(
-        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-      );
-    });
-  document
-    .querySelector(".append-2-slides")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.appendSlide([
-        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-      ]);
-    });
-
-   //要素の取得とスピードの設定
-  var box = $('.colorbox'),
-  speed = 700;
-
-  //.colorboxの付いた全ての要素に対して下記の処理を行う
-  box.each(function(){
-  $(this).append('<div class="color"></div>')
-  var color = $(this).find($('.color')),
-  image = $(this).find('img');
-  var counter = 0;
-
-  image.css('opacity','0');
-  color.css('width','0%');
-  //inviewを使って背景色が画面に現れたら処理をする
-  color.on('inview', function(){
-      if(counter == 0){
-  $(this).delay(200).animate({'width':'100%'},speed,function(){
-                image.css('opacity','1');
-                $(this).css({'left':'0' , 'right':'auto'});
-                $(this).animate({'width':'0%'},speed);
-              })
-          counter = 1;
-        }
- });
-});
 });
 
 $(function () {
